1f8719d0899211b2dd592d27bf7321dd6c9b7955
Anton Fedorov <avfedorov235@gmail.com>
2021-10-25 00:09:08 +0300

    +sprint6/spring-config

--- spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
@@ -1,8 +1,5 @@
 package ru.sber.services
 
-import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
-
 class FirstService {
     override fun toString(): String {
         return "I am firstService"
@@ -15,14 +12,12 @@ class SecondService {
     }
 }
 
-@Component
 class ThirdService {
     override fun toString(): String {
         return "I am thirdService"
     }
 }
 
-@Service
 class FourthService {
     override fun toString(): String {
         return "I am fourthService"
